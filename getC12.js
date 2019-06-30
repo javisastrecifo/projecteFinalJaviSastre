02e1608fe27e5416b1c969e7885f402270418b42
JaviSastre <javisastrecifo@gmail.com>
2019-06-30 16:00:52 +0200

    Procés d'encode i de decode acabats i funcionant

--- getC12.js
+++ getC12.js
@@ -2,7 +2,7 @@
  * 
  */
 
-function getC12 (){
-	var arrayC12 = [[2,2,1],[5,7,2],[5,5,4]];
-	return 	arrayC12;
+function getC12() {
+	var arrayC12 = [ [ 2, 2, 1 ], [ 5, 7, 2 ], [ 5, 5, 4 ] ];
+	return arrayC12;
 }
